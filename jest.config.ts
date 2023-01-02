f85bd93eaecca9e153f15f9ce689bbbd89c285f1
rafael-correa98 <rafaelcorrean98@gmail.com>
2023-01-02 19:34:43 -0300

    config jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        ".+\\.ts$": "ts-jest",
+    },
+    roots: ["<rootDir>/tests"],
+    collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
+    coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+    };
